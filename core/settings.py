93c5690b06e19674c90f2a5be8cb64a55256e491
rubbiekelvin <rubbiekelvinvoltsman@gmail.com>
2024-12-13 14:20:50 +0100

    chore: allow host

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-511b-fssa*wno($l032q4udp&w9=)4&av!j!%80k8eqv2$^htp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
